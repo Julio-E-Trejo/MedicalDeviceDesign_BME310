9643c6d38d5187ee9bb2da5b8a275454bd26bdd1
Julio-E-Trejo <65774639+Julio-E-Trejo@users.noreply.github.com>
2023-03-29 11:33:26 -0700

    Add files via upload

---
+++ Address_ESP8266/ESP8266WIFI.ino
@@ -0,0 +1,25 @@
+
+const char* ssid = "eduroam"; //Enter SSID
+const char* password = "rtjulio119"; //Enter Password
+
+void setup(void)
+{ 
+  Serial.begin(115200);
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+     delay(500);
+     Serial.print("*");
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connection Successful");
+  Serial.print("The IP Address of ESP8266 Module is: ");
+  Serial.print(WiFi.localIP());// Print the IP address
+}
+
+void loop() 
+{
+  // EMPTY
+}
